74ddada9ac1267c434a3539c2fcc3a23a5b93341
aleawaludin06 <aleawaludin06@gmail.com>
2026-04-15 14:45:22 +0700

    Add files via upload

---
+++ Tugas_8/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Index</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <?php include 'menu.php'; ?>
+    </div>
+</body>
+</html>
